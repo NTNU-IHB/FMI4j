55ba3229c7d19497547025d549ccb8b91e8b1bf8
Lars Ivar Hatledal <laht@hials.no>
2018-03-23 21:09:38 +0100

    refactor packages, clean up tests

---
+++ fmi-import/src/main/kotlin/no/mechatronics/sfi/fmi4j/fmu/proxy/v2/Fmi2LibraryWrapper.kt
@@ -0,0 +1,401 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017-2018 Norwegian University of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING  FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+package no.mechatronics.sfi.fmi4j.fmu.proxy.v2
+
+import com.sun.jna.Memory
+import com.sun.jna.Pointer
+import no.mechatronics.sfi.fmi4j.common.*
+import no.mechatronics.sfi.fmi4j.fmu.misc.ArrayBuffers
+import no.mechatronics.sfi.fmi4j.fmu.misc.FmiBoolean
+import no.mechatronics.sfi.fmi4j.fmu.misc.FmuState
+import no.mechatronics.sfi.fmi4j.fmu.misc.LibraryProvider
+import no.mechatronics.sfi.fmi4j.fmu.proxy.v2.structs.Fmi2CallbackFunctions
+import no.mechatronics.sfi.fmi4j.modeldescription.variables.StringArray
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+
+/**
+ * @author Lars Ivar Hatledal
+ */
+abstract class Fmi2LibraryWrapper<out E : Fmi2Library>(
+        protected var c: Pointer,
+        private val libraryProvider: LibraryProvider<E>
+) {
+
+    private companion object {
+        val LOG: Logger = LoggerFactory.getLogger(Fmi2LibraryWrapper::class.java)
+    }
+
+    private val functions: Fmi2CallbackFunctions = Fmi2CallbackFunctions()
+    private val buffers: ArrayBuffers = ArrayBuffers()
+
+    var instanceFreed = false
+        private set
+
+    /**
+     * The status returned from the last call to a FMU function
+     */
+    var lastStatus: FmiStatus = FmiStatus.NONE
+        private set
+
+    /**
+     * Has terminate been called on the FMU?
+     */
+    var isTerminated: Boolean = false
+        private set
+
+    protected val library: E
+        get() = libraryProvider.get()
+
+
+    protected fun updateStatus(status: Int): FmiStatus {
+        return updateStatus(FmiStatus.valueOf(status))
+    }
+
+    protected fun updateStatus(status: FmiStatus): FmiStatus {
+        return status.also { lastStatus = it }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2GetTypesPlatform
+     */
+    val typesPlatform: String = library.fmi2GetTypesPlatform()
+
+    /**
+     *
+     * @see Fmi2Library.fmi2GetVersion()
+     */
+    val version: String = library.fmi2GetVersion()
+
+
+    /**
+     * @see Fmi2Library.fmi2SetDebugLogging
+     */
+    fun setDebugLogging(loggingOn: Boolean, nCategories: Int, categories: StringArray): FmiStatus {
+        return updateStatus(library.fmi2SetDebugLogging(c,
+                FmiBoolean.convert(loggingOn), nCategories, categories))
+    }
+
+    /**
+     * @see Fmi2Library.fmi2SetupExperiment
+     */
+    fun setupExperiment(toleranceDefined: Boolean, tolerance: Double, startTime: Double, stopTimeDefined: Boolean, stopTime: Double): FmiStatus {
+        return updateStatus(library.fmi2SetupExperiment(c,
+                FmiBoolean.convert(toleranceDefined), tolerance,
+                startTime, FmiBoolean.convert(stopTimeDefined), stopTime))
+    }
+
+    /**
+     * @see Fmi2Library.fmi2EnterInitializationMode
+     */
+    fun enterInitializationMode(): FmiStatus {
+        return updateStatus(library.fmi2EnterInitializationMode(c))
+    }
+
+    /**
+     * @see Fmi2Library.fmi2ExitInitializationMode
+     */
+    fun exitInitializationMode(): FmiStatus {
+        return updateStatus(library.fmi2ExitInitializationMode(c))
+    }
+
+    /**
+     * @see Fmi2Library.fmi2Terminate
+     */
+    @JvmOverloads
+    fun terminate(freeInstance: Boolean = true): FmiStatus {
+
+        if (!isTerminated) {
+
+            return try {
+                updateStatus(library.fmi2Terminate(c))
+            } catch (ex: Error) {
+                LOG.error("Error caught on fmi2Terminate: ${ex.javaClass.simpleName}")
+                updateStatus(FmiStatus.OK)
+            } finally {
+                isTerminated = true
+                if (freeInstance) {
+                    freeInstance()
+                }
+            }
+
+        } else {
+            LOG.warn("Terminated has already been called..")
+            return FmiStatus.Discard
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2FreeInstance
+     */
+    internal fun freeInstance() {
+        if (!instanceFreed) {
+            var success = false
+            try {
+                library.fmi2FreeInstance(c)
+                success = true
+            } catch (ex: Error) {
+                LOG.error("Error caught on fmi2FreeInstance: ${ex.javaClass.simpleName}")
+            } finally {
+                val msg = if (success) "successfully" else "unsuccessfully"
+                LOG.debug("Instance freed $msg")
+                instanceFreed = true
+            }
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2Reset
+     */
+    fun reset(): FmiStatus = updateStatus(library.fmi2Reset(c))
+
+    /**
+     * @see Fmi2Library.fmi2GetInteger
+     */
+    fun getInteger(valueReference: Int): FmuIntegerRead {
+        return with(buffers) {
+            vr[0] = valueReference
+            library.fmi2GetInteger(c, vr, vr.size, iv).let {
+                FmuIntegerRead(iv[0], updateStatus(it))
+            }
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2GetInteger
+     */
+    @JvmOverloads
+    fun getInteger(vr: IntArray, value: IntArray = IntArray(vr.size)): FmuIntegerArrayRead {
+        return library.fmi2GetInteger(c, vr, vr.size, value).let {
+            FmuIntegerArrayRead(value, updateStatus(it))
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2GetReal
+     */
+    fun getReal(valueReference: Int): FmuRealRead {
+        return with(buffers) {
+            vr[0] = valueReference
+            library.fmi2GetReal(c, vr, vr.size, rv).let {
+                FmuRealRead(rv[0], updateStatus(it))
+            }
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2GetReal
+     */
+    @JvmOverloads
+    fun getReal(vr: IntArray, value: DoubleArray = DoubleArray(vr.size)): FmuRealArrayRead {
+        return library.fmi2GetReal(c, vr, vr.size, value).let {
+            FmuRealArrayRead(value, updateStatus(it))
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2GetString
+     */
+    fun getString(valueReference: Int): FmuStringRead {
+        return with(buffers) {
+            vr[0] = valueReference
+            library.fmi2GetString(c, vr, vr.size, sv).let {
+                FmuStringRead(sv[0], updateStatus(it))
+            }
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2GetString
+     */
+    @JvmOverloads
+    fun getString(vr: IntArray, value: StringArray = StringArray(vr.size, { "" })): FmuStringArrayRead {
+        return (library.fmi2GetString(c, vr, vr.size, value)).let {
+            FmuStringArrayRead(value, updateStatus(it))
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2GetBoolean
+     */
+    fun getBoolean(valueReference: Int): FmuBooleanRead {
+        return with(buffers) {
+            vr[0] = valueReference
+            library.fmi2GetBoolean(c, vr, vr.size, iv).let {
+                FmuBooleanRead(FmiBoolean.convert(iv[0]), updateStatus(it))
+            }
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2GetBoolean
+     */
+    @JvmOverloads
+    fun getBoolean(vr: IntArray, value: BooleanArray = BooleanArray(vr.size)): FmuBooleanArrayRead {
+        val intArray = value.map { FmiBoolean.convert(it) }.toIntArray()
+        return library.fmi2GetBoolean(c, vr, vr.size, intArray).let {
+            for ((i, v) in intArray.withIndex()) {
+                value[i] = FmiBoolean.convert(v)
+            }
+            FmuBooleanArrayRead(value, updateStatus(it))
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2GetBoolean
+     */
+    fun getBoolean(vr: IntArray, value: IntArray): FmuIntegerArrayRead {
+        return library.fmi2GetBoolean(c, vr, vr.size, value).let {
+            FmuIntegerArrayRead(value, updateStatus(it))
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2SetInteger
+     */
+    fun setInteger(valueReference: Int, value: Int): FmiStatus {
+        return with(buffers) {
+            vr[0] = valueReference
+            iv[0] = value
+            setInteger(vr, iv)
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2SetInteger
+     */
+    fun setInteger(vr: IntArray, value: IntArray): FmiStatus {
+        return updateStatus((library.fmi2SetInteger(c, vr, vr.size, value)))
+    }
+
+    /**
+     * @see Fmi2Library.fmi2SetReal
+     */
+    fun setReal(valueReference: Int, value: Double): FmiStatus {
+        return with(buffers) {
+            vr[0] = valueReference
+            rv[0] = value
+            setReal(vr, rv)
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2SetReal
+     */
+    fun setReal(vr: IntArray, value: DoubleArray): FmiStatus {
+        return updateStatus((library.fmi2SetReal(c, vr, vr.size, value)))
+    }
+
+    /**
+     * @see Fmi2Library.fmi2SetString
+     */
+    fun setString(valueReference: Int, value: String): FmiStatus {
+        return with(buffers) {
+            vr[0] = valueReference
+            sv[0] = value
+            setString(vr, sv)
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2SetString
+     */
+    fun setString(vr: IntArray, value: StringArray): FmiStatus {
+        return updateStatus((library.fmi2SetString(c, vr, vr.size, value)))
+    }
+
+    /**
+     * @see Fmi2Library.fmi2SetBoolean
+     */
+    fun setBoolean(valueReference: Int, value: Boolean): FmiStatus {
+        return with(buffers) {
+            vr[0] = valueReference
+            iv[0] = FmiBoolean.convert(value)
+            setBoolean(vr, iv)
+        }
+    }
+
+    /**
+     * @see Fmi2Library.fmi2SetBoolean
+     */
+    fun setBoolean(vr: IntArray, value: IntArray): FmiStatus {
+        return updateStatus(library.fmi2SetBoolean(c, vr, vr.size, value))
+    }
+
+    /**
+     * @see Fmi2Library.fmi2SetBoolean
+     */
+    fun setBoolean(vr: IntArray, value: BooleanArray): FmiStatus {
+        return setBoolean(vr, value.map { if (it) 1 else 0 }.toIntArray())
+    }
+
+    fun getDirectionalDerivative(vUnknown_ref: IntArray, vKnown_ref: IntArray, dvKnown: DoubleArray, dvUnknown: DoubleArray): FmiStatus {
+        return updateStatus(library.fmi2GetDirectionalDerivative(c,
+                vUnknown_ref, vUnknown_ref.size, vKnown_ref, vKnown_ref.size, dvKnown, dvUnknown))
+    }
+
+    fun getFMUState(fmuState: FmuState): FmuState {
+        return fmuState.also {
+            updateStatus(library.fmi2GetFMUstate(c, fmuState))
+        }
+    }
+
+    fun setFMUState(fmuState: FmuState): FmiStatus {
+        return updateStatus(library.fmi2SetFMUstate(c, fmuState.pointer))
+    }
+
+    fun freeFMUState(fmuState: FmuState): FmiStatus {
+        return updateStatus(library.fmi2FreeFMUstate(c, fmuState))
+    }
+
+    fun serializedFMUStateSize(fmuState: FmuState): Int {
+        val memory = Memory(Pointer.SIZE.toLong())
+        updateStatus(library.fmi2SerializedFMUstateSize(c, fmuState.pointer, memory))
+        return memory.getInt(0)
+    }
+
+    fun serializeFMUState(fmuState: FmuState): ByteArray {
+        val size = serializedFMUStateSize(fmuState)
+        return ByteArray(size).also {
+            updateStatus(library.fmi2SerializeFMUstate(c, fmuState.pointer, it, size))
+        }
+    }
+
+    fun deSerializeFMUState(serializedState: ByteArray): FmuState {
+        return FmuState().also { state ->
+            updateStatus(library.fmi2DeSerializeFMUstate(c, serializedState, serializedState.size, state))
+        }
+    }
+
+}
+
+
+
+
+
+
+
---
+++ fmi-import/src/main/kotlin/no/mechatronics/sfi/fmi4j/fmu/proxy/v2/cs/Fmi2CoSimulationLibrary.kt
@@ -0,0 +1,147 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017-2018 Norwegian University of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING  FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package no.mechatronics.sfi.fmi4j.fmu.proxy.v2.cs
+
+import com.sun.jna.Pointer
+import com.sun.jna.ptr.DoubleByReference
+import com.sun.jna.ptr.IntByReference
+import no.mechatronics.sfi.fmi4j.fmu.misc.StringByReference
+import no.mechatronics.sfi.fmi4j.fmu.proxy.v2.Fmi2Library
+
+/**
+ *
+ * @author Lars Ivar Hatledal
+ */
+interface Fmi2CoSimulationLibrary : Fmi2Library {
+
+    /**
+     * Sets the n-th time derivative of real input variables. Argument “vr” is a vector of value
+     * references that define the variables whose derivatives shall be set. The array “order”
+     * contains the orders of the respective derivative (1 means the first derivative, 0 is not
+     * allowed). Argument “value” is a vector with the values of the derivatives. “nvr” is the
+     * dimension of the vectors.
+     * Restrictions on using the function are the same as for the fmi2SetReal function.
+     */
+    fun fmi2SetRealInputDerivatives(c: Pointer, vr: IntArray, nvr: Int, order: IntArray, value: DoubleArray): Int
+
+    /**
+     * Retrieves the n-th derivative of output values. Argument “vr” is a vector of “nvr” value
+     * references that define the variables whose derivatives shall be retrieved. The array “order”
+     * contains the order of the respective derivative (1 means the first derivative, 0 is not allowed).
+     * Argument “value” is a vector with the actual values of the derivatives.
+     * Restrictions on using the function are the same as for the fmi2GetReal function.
+     */
+    fun fmi2GetRealOutputDerivatives(c: Pointer, vr: IntArray, nvr: Int, order: IntArray, value: DoubleArray): Int
+
+    /**
+     * The computation of a time step is started.
+     * Argument currentCommunicationPoint is the current communication point of the master
+     * (𝑡𝑐𝑖) and argument communicationStepSize is the communication step size (ℎ𝑐𝑖). The
+     * latter must be > 0.0. The slave must integrate until time instant 𝑡𝑐𝑖+1 = 𝑡𝑐𝑖 + ℎ𝑐𝑖. [The calling
+     * environment defines the communication points and fmi2DoStep must synchronize to these
+     * points by always integrating exactly to 𝑡𝑐𝑖 + ℎ𝑐𝑖. It is up to fmi2DoStep how to achieve this.]
+     * At the first call to fmiDoStep after fmi2ExitInitializationMode was called
+     * currentCommunicationPoint must be equal to startTime as set with
+     * fmi2SetupExperiment. [Formally argument currentCommunicationPoint is not needed.
+     * It is present in order to handle a mismatch between the master and the FMU state of the
+     * slave: The currentCommunicationPoint and the FMU state of the slaves defined by
+     * former fmi2DoStep or fmi2SetFMUState calls have to be consistent with respect to each
+     * other. For example, if the slave does not use the update formula for the independent variable
+     * as required above, 𝑡𝑐𝑖+1 = 𝑡𝑐𝑖 + ℎ𝑐𝑖 (using argument 𝑡𝑐𝑖 = currentCommunicationPoint of
+     * fmi2DoStep) but uses internally an own update formula, such as 𝑡𝑐𝑠,𝑖+1 = 𝑡𝑐𝑠,𝑖 + ℎ𝑐𝑠,𝑖 then
+     * the slave could use as time increment ℎ𝑐𝑠,𝑖: = (𝑡𝑐𝑖 − 𝑡𝑐𝑠,𝑖) + ℎ𝑐𝑖 (instead of ℎ𝑐𝑠,𝑖: = ℎ𝑐𝑖,) to
+     * avoid a mismatch between the master time 𝑡𝑐𝑖+1 and the slave internal time 𝑡𝑐𝑠,𝑖+1 for large i.]
+     * Argument noSetFMUStatePriorToCurrentPoint is fmi2True if fmi2SetFMUState will
+     * no longer be called for time instants prior to currentCommunicationPoint in this
+     * simulation run [the slave can use this flag to flush a result buffer].
+     * The function returns:
+     * fmi2OK - if the communication step was computed successfully until its end.
+     * fmi2Discard – if the slave computed successfully only a subinterval of the communication
+     * step. The master can call the appropriate fmi2GetXXXStatus functions to get further
+     * information. If possible, the master should retry the simulation with a shorter communication
+     * step size. [Redoing a step is only possible if the FMU state has been recorded at the
+     * beginning of the current (failed) step with fmi2GetFMUState. Redoing a step is performed
+     * by calling fmi2SetFMUState and afterwards calling fmi2DoStep with the new
+     * communicationStepSize. Note, it is not possible to change
+     * currentCommunicationPoint in such a call.]
+     * fmi2Error – the communication step could not be carried out at all. The master can try to
+     * repeat the step with other input values and/or a different communication step size in the
+     * same way as described in the fmi2Discard case above.
+     * fmi2Fatal – if an error occurred which corrupted the FMU irreparably. [The master should
+     * stop the simulation run immediatlely.] See section 2.1.3 for details.
+     * fmi2Pending – is returned if the slave executes the function asynchronously. That means
+     * the slave starts the computation but returns immediately. The master has to call
+     * fmi2GetStatus(...,fmi2DoStep,...) to find out, if the slave is done. An alternative is to
+     * wait until the callback function fmi2StepFinished is called by the slave. fmi2CancelStep
+     * can be called to cancel the current computation. It is not allowed to call any other function
+     * during a pending fmi2DoStep.
+     */
+    fun fmi2DoStep(c: Pointer, currentCommunicationPoint: Double, communicationStepSize: Double, noSetFMUStatePriorToCurrent: Int): Int
+
+    /**
+     * Can be called if fmi2DoStep returned fmi2Pending in order to stop the current
+     * asynchronous execution. The master calls this function if for example the co-simulation run is
+     * stopped by the user or one of the slaves. Afterwards it is only allowed to call fmi2Reset or
+     * fmi2FreeInstance.
+     */
+    fun fmi2CancelStep(c: Pointer): Int
+
+    /**
+     * Informs the master about the actual status of the simulation run. Which status information is
+     * to be returned is specified by the argument fmi2StatusKind
+     */
+    fun fmi2GetStatus(c: Pointer, s: Int, value: IntByReference): Int
+
+    /**
+     * Informs the master about the actual status of the simulation run. Which status information is
+     * to be returned is specified by the argument fmi2StatusKind
+     */
+    fun fmi2GetRealStatus(c: Pointer, s: Int, value: DoubleByReference): Int
+
+    /**
+     * Informs the master about the actual status of the simulation run. Which status information is
+     * to be returned is specified by the argument fmi2StatusKind
+     */
+    fun fmi2GetIntegerStatus(c: Pointer, s: Int, value: IntByReference): Int
+
+    /**
+     * Informs the master about the actual status of the simulation run. Which status information is
+     * to be returned is specified by the argument fmi2StatusKind
+     */
+    fun fmi2GetBooleanStatus(c: Pointer, s: Int, value: IntByReference): Int
+
+    /**
+     * Informs the master about the actual status of the simulation run. Which status information is
+     * to be returned is specified by the argument fmi2StatusKind
+     */
+    fun fmi2GetStringStatus(c: Pointer, s: Int, value: StringByReference): Int
+
+    /**
+     * Extension method
+     */
+    fun fmi2GetMaxStepsize(c: Pointer, value: DoubleByReference): Int
+
+}
+
