949ba86a036689381e4edcbb33994924ddfbf45d
Lars Ivar Hatledal <laht@hials.no>
2018-04-30 20:41:48 +0200

    fix wrong import in SerializeTest

--- fmi-modeldescription/src/test/kotlin/no/mechatronics/sfi/fmi4j/modeldescription/SerializeTest.kt
+++ fmi-modeldescription/src/test/kotlin/no/mechatronics/sfi/fmi4j/modeldescription/SerializeTest.kt
@@ -1,15 +1,11 @@
 package no.mechatronics.sfi.fmi4j.modeldescription
 
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream
 import org.junit.Assert
 import org.junit.BeforeClass
 import org.junit.Test
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.io.ByteArrayInputStream
-import java.io.File
-import java.io.ObjectInputStream
-import java.io.ObjectOutputStream
+import java.io.*
 
 class SerializeTest {
 
@@ -34,58 +30,18 @@ class SerializeTest {
     @Test
     fun test1() {
 
-        val bos = ByteOutputStream()
+        val bos = ByteArrayOutputStream()
         ObjectOutputStream(bos).use {
             it.writeObject(modelDescription)
             it.flush()
         }
 
-        ObjectInputStream(ByteArrayInputStream(bos.bytes)).use {
+        ObjectInputStream(ByteArrayInputStream(bos.toByteArray())).use {
             val md: CommonModelDescription = it.readObject() as CommonModelDescription
             md.modelVariables.variables.forEach { LOG.info("$it") }
             LOG.info("${md.modelStructure}")
         }
 
-
     }
-
-//    class VariableAdapter: JsonDeserializer<AbstractTypedScalarVariable<*>>, JsonSerializer<AbstractTypedScalarVariable<*>> {
-//        override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext?): AbstractTypedScalarVariable<*> {
-//            println(json)
-//            println(typeOfT)
-//            println(context)
-//            return IntegerVariable(ScalarVariableImpl())
-//        }
-//
-//        override fun serialize(src: AbstractTypedScalarVariable<*>?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
-//            val gson = Gson()
-//            return when(src) {
-//                is IntegerVariable -> gson.toJsonTree(src)
-//                is RealVariable -> gson.toJsonTree(src)
-//                is StringVariable -> gson.toJsonTree(src)
-//                is BooleanVariable -> gson.toJsonTree(src)
-//                is EnumerationVariable -> gson.toJsonTree(src)
-//                else -> JsonObject()
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun testGson() {
-//
-//        val gson = GsonBuilder()
-//                .setPrettyPrinting()
-//                .registerTypeAdapter(AbstractTypedScalarVariable::class.java, VariableAdapter())
-//                .create()
-//
-//
-//
-//        val json = gson.toJson(modelDescription)
-//        println(json)
-//        val md = gson.fromJson(json, ModelDescriptionImpl::class.java)
-//
-//        println(md)
-//
-//    }
 
 }
