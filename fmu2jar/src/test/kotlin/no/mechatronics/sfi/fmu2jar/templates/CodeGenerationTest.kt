a5883386dedd9c85a93d62444a874d59ce407f6c
Lars Ivar Hatledal <laht@hials.no>
2018-06-15 09:06:30 +0200

    Add TestUtils also to fmu2jar

--- fmu2jar/src/test/kotlin/no/mechatronics/sfi/fmu2jar/templates/CodeGenerationTest.kt
+++ fmu2jar/src/test/kotlin/no/mechatronics/sfi/fmu2jar/templates/CodeGenerationTest.kt
@@ -1,11 +1,11 @@
 package no.mechatronics.sfi.fmu2jar.templates
 
 
-import no.mechatronics.sfi.fmi4j.TestUtils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.File
 import no.mechatronics.sfi.fmi4j.modeldescription.ModelDescriptionParser
+import no.mechatronics.sfi.fmu2jar.TestUtils
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable
@@ -20,7 +20,8 @@ class CodeGenerationTest {
     @Test
     fun generateBody() {
 
-        val file = File(TestUtils.getTEST_FMUs(), "FMI_2.0/CoSimulation/${TestUtils.getOs()}/20sim/4.6.4.8004/ControlledTemperature/ControlledTemperature.fmu")
+        val file = File(TestUtils.getTEST_FMUs(),
+                "FMI_2.0/CoSimulation/${TestUtils.getOs()}/20sim/4.6.4.8004/ControlledTemperature/ControlledTemperature.fmu")
         Assertions.assertTrue(file.exists())
         val md = ModelDescriptionParser.parse(file)
         LOG.info(CodeGeneration.generateWrapper(md) )
