d0bf9a54ad5eb34fb6fe241a27dd8380e16229bc
Lars Ivar Hatledal <laht@hials.no>
2018-07-05 11:43:47 +0200

    Prefer own class files

--- fmi-modeldescription/src/main/kotlin/no/mechatronics/sfi/fmi4j/modeldescription/misc/LogCategories.kt
+++ fmi-modeldescription/src/main/kotlin/no/mechatronics/sfi/fmi4j/modeldescription/misc/LogCategories.kt
@@ -1,34 +1,32 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017-2018 Norwegian University of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING  FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package no.mechatronics.sfi.fmi4j.modeldescription.misc
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 import java.io.Serializable
-
-
-class LogCategories : Iterable<LogCategory>, Serializable {
-
-    val size
-        get() = categories.size
-
-    override fun iterator(): Iterator<LogCategory> = categories.iterator()
-
-    @JacksonXmlProperty(localName = "Category")
-    @JacksonXmlElementWrapper(useWrapping = false)
-    private val _categories: List<LogCategory>? = null
-
-    private val categories: List<LogCategory>
-        get() = _categories ?: emptyList()
-
-    operator fun contains(category: LogCategory) = categories.contains(category)
-
-    operator fun contains(category: String) = categories.map { it.name }.contains(category)
-
-    override fun toString(): String {
-        return "LogCategoriesImpl(size=$size, categories=$categories)"
-    }
-
-}
-
 
 /**
  * @author Lars Ivar Hatledal
@@ -42,6 +40,30 @@ data class LogCategory(
         val description: String? = null
 
 ) : Serializable
+
+/**
+ * @author Lars Ivar Hatledal
+ */
+class LogCategories : Iterable<LogCategory>, Serializable {
+
+    val size
+        get() = categories.size
+
+    override fun iterator(): Iterator<LogCategory> = categories.iterator()
+
+    @JacksonXmlProperty(localName = "Category")
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private val categories: List<LogCategory> = emptyList()
+
+    operator fun contains(category: LogCategory) = categories.contains(category)
+
+    operator fun contains(category: String) = categories.map { it.name }.contains(category)
+
+    override fun toString(): String {
+        return "LogCategoriesImpl(size=$size, categories=$categories)"
+    }
+
+}
 
 /**
  * Log all events (during initialization and simulation).
