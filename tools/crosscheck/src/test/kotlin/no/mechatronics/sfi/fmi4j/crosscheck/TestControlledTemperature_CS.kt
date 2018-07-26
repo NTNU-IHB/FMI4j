c60d6ae7afd2d15778867c54c689559dd4103677
Lars Ivar Hatledal <laht@hials.no>
2018-07-26 23:20:20 +0200

    add crosscheck

---
+++ tools/crosscheck/src/test/kotlin/no/mechatronics/sfi/fmi4j/crosscheck/TestControlledTemperature_CS.kt
@@ -0,0 +1,42 @@
+package no.mechatronics.sfi.fmi4j.crosscheck
+
+import no.mechatronics.sfi.fmu2jar.TEST_FMUs
+import no.mechatronics.sfi.fmu2jar.currentOS
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.File
+
+@EnabledIfEnvironmentVariable(named = "TEST_FMUs", matches = ".*")
+class TestControlledTemperature_CS {
+
+    companion object {
+        val LOG: Logger = LoggerFactory.getLogger(TestControlledTemperature_CS::class.java)
+    }
+
+    @Test
+    fun test() {
+
+        val path = "$TEST_FMUs/FMI_2.0/CoSimulation/$currentOS/20sim/4.6.4.8004/ControlledTemperature/ControlledTemperature.fmu"
+        Assertions.assertTrue(File(path).exists())
+
+        val args = arrayOf(
+                "-fmu", "\"$path\"",
+                "-dt", "1E-4",
+                "-stop", "5",
+                "-vars", "Temperature_Reference, Temperature_Room"
+        )
+
+        FmuDriver.main(args)
+
+        File("ControlledTemperature_out.csv").apply {
+            if (exists()) {
+                delete()
+            }
+        }
+
+    }
+
+}
