1825fb6b174acfc77650b77799ec257da7851068
Lars Ivar Hatledal <laht@ntnu.no>
2018-08-14 12:24:26 +0200

    refactor

--- library/FMI4j/fmi-import/src/test/kotlin/no/mechatronics/sfi/fmi4j/importer/misc/CompareWithJavaFmi.kt
+++ library/FMI4j/fmi-import/src/test/kotlin/no/mechatronics/sfi/fmi4j/importer/misc/CompareWithJavaFmi.kt
@@ -24,7 +24,8 @@ class CompareWithJavaFmi {
         private val LOG: Logger = LoggerFactory.getLogger(CompareWithJavaFmi::class.java)
     }
 
-    private val path = "${TestUtils.getTEST_FMUs()}/FMI_2.0/CoSimulation/${TestUtils.getOs()}/20sim/4.6.4.8004/ControlledTemperature/ControlledTemperature.fmu"
+    private val path = "${TestUtils.getTEST_FMUs()}/FMI_2.0/CoSimulation/${TestUtils.getOs()}" +
+            "/20sim/4.6.4.8004/ControlledTemperature/ControlledTemperature.fmu"
 
     private val fmu = Fmu.from(File(path))
 
