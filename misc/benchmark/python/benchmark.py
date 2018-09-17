836b6497cb0477e9c6545dd83dd7c7a00e0ea2df
Lars Ivar Hatledal <laht@ntnu.no>
2018-09-17 11:00:22 +0200

    update python benchmark code

--- misc/benchmark/python/benchmark.py
+++ misc/benchmark/python/benchmark.py
@@ -1,8 +1,17 @@
 import datetime
 import os
+import platform
 import shutil
 from fmpy import read_model_description, extract
 from fmpy.fmi2 import FMU2Slave
+
+
+def getOs():
+    sys = platform.system()
+    if sys == "Windows":
+        return "win64"
+    else:
+        return "linux64"
 
 
 class TestOptions:
@@ -15,18 +24,23 @@ class TestOptions:
 
 TEST_FMUs = os.environ.get('TEST_FMUs')
 
-options = TestOptions("{}/FMI_2.0/CoSimulation/win64/FMUSDK/2.0.4/BouncingBall/bouncingBall.fmu".format(TEST_FMUs),
-                      1E-2, 100, 0)
+options = [
 
+    TestOptions(
+        "{}/FMI_2.0/CoSimulation/{}/20sim/4.6.4.8004/ControlledTemperature/ControlledTemperature.fmu".format(TEST_FMUs, getOs()),1E-4, 10, 47),
+    TestOptions(
+        "{}/FMI_2.0/CoSimulation/{}/20sim/4.6.4.8004/TorsionBar/TorsionBar.fmu".format(TEST_FMUs, getOs()), 1E-5, 12, 2)
 
-# options = TestOptions("{}/FMI_2.0/CoSimulation/win64/20sim/4.6.4.8004/TorsionBar/TorsionBar.fmu", 1E-5, 12, 2)
+]
 
 
 def main():
 
-    model_description = read_model_description(options.fmu_filename)
+    option = options[1]
 
-    unzipdir = extract(options.fmu_filename)
+    model_description = read_model_description(option.fmu_filename)
+
+    unzipdir = extract(option.fmu_filename)
 
     fmu = FMU2Slave(guid=model_description.guid,
                     unzipDirectory=unzipdir,
@@ -35,27 +49,26 @@ def main():
 
     # initialize
     fmu.instantiate()
-    fmu.setupExperiment(tolerance=1E-4, startTime=0.0, stopTime=options.stop_time)
+    fmu.setupExperiment(tolerance=1E-4, startTime=0.0, stopTime=option.stop_time)
     fmu.enterInitializationMode()
     fmu.exitInitializationMode()
 
     start = datetime.datetime.now()
 
+    i = 0
     t = 0.0
     sum = 0.0
-    i = 0
     # simulation loop
-    while t < options.stop_time-options.step_size:
+    while t <= (option.stop_time-option.step_size):
 
         i += 1
         # perform one step
-        fmu.doStep(currentCommunicationPoint=t, communicationStepSize=options.step_size)
+        fmu.doStep(currentCommunicationPoint=t, communicationStepSize=option.step_size)
 
-        sum += fmu.getReal([options.vr])[0]
-        #print("h={}".format(h))
+        sum += fmu.getReal([option.vr])[0]
 
         # advance the time
-        t += options.step_size
+        t += option.step_size
 
     end = datetime.datetime.now()
     print("sum={}, iter={}".format(sum, i))
