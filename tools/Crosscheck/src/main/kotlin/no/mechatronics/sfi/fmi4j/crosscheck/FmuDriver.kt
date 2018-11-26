6b5ab42359c479bf9dc24cea3eb75c87bd8b8b13
Lars Ivar Hatledal <laht@ntnu.no>
2018-11-26 12:39:59 +0100

    update fmudriver

--- tools/Crosscheck/src/main/kotlin/no/mechatronics/sfi/fmi4j/crosscheck/FmuDriver.kt
+++ tools/Crosscheck/src/main/kotlin/no/mechatronics/sfi/fmi4j/crosscheck/FmuDriver.kt
@@ -160,9 +160,11 @@ object FmuDriver {
             return """
 
             The cross-check results have been generated with FMI4j's FmuDriver.
-            To get more information download the 'fmudriver' tool from https://github.com/SFI-Mechatronics/FMI4j/releases and run
+            To get more information download the 'fmudriver' tool from https://github.com/SFI-Mechatronics/FMI4j/releases and run:
 
+            ```
             java -jar fmudriver.jar -h
+            ```
 
             """.trimIndent()
 
