e5da530647077ff6daa4786286b388cc1d8e1557
Lars Ivar Hatledal <laht@ntnu.no>
2019-01-09 10:56:09 +0100

    only run test if env vars are presents

--- fmi-import/src/test/kotlin/no/ntnu/ihb/fmi4j/modeldescription/vendors/AMESim/AMESimTest.kt
+++ fmi-import/src/test/kotlin/no/ntnu/ihb/fmi4j/modeldescription/vendors/AMESim/AMESimTest.kt
@@ -2,21 +2,21 @@ package no.ntnu.ihb.fmi4j.modeldescription.vendors.AMESim
 
 import no.ntnu.ihb.fmi4j.TestFMUs
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+@EnabledIfEnvironmentVariable(named = "TEST_FMUs", matches = ".*")
 class AMESimTest {
 
     private companion object {
-
         val LOG: Logger = LoggerFactory.getLogger(AMESimTest::class.java)
-
     }
 
     @Test
     fun test() {
 
-       no.ntnu.ihb.fmi4j.TestFMUs.fmi20().cs()
+       TestFMUs.fmi20().cs()
                 .vendor("AMESim")
                 .version("15")
                 .fmu("MIS_cs").use {
