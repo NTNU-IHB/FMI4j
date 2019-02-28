154ce79df0f3f8f2043f765a0368ddafc779cb16
Lars Ivar Hatledal <laht@hials.no>
2019-02-28 16:35:04 +0100

    improve crosscheck

---
+++ fmu-driver/src/test/kotlin/no/ntnu/ihb/fmi4j/fmudriver/crosscheck/RunCrossCheck.kt
@@ -0,0 +1,267 @@
+package no.ntnu.ihb.fmi4j.fmudriver.crosscheck
+
+import no.ntnu.ihb.fmi4j.fmudriver.DriverOptions
+import no.ntnu.ihb.fmi4j.fmudriver.Failure
+import no.ntnu.ihb.fmi4j.fmudriver.FmuDriver
+import no.ntnu.ihb.fmi4j.fmudriver.Rejection
+import no.ntnu.ihb.fmi4j.modeldescription.ModelDescriptionProvider
+import no.ntnu.ihb.fmi4j.modeldescription.jacskon.JacksonModelDescriptionParser
+import no.ntnu.ihb.fmi4j.util.OsUtil
+import org.junit.jupiter.api.condition.OS
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.File
+import java.util.concurrent.atomic.AtomicInteger
+
+private const val FMI4j_VERSION = "0.18.0"
+
+private const val README = """
+        The cross-check results have been generated with FMI4j's FmuDriver.
+        To get more information download the 'fmudriver' tool from https://github.com/NTNU-IHB/FMI4j/releases and run:
+
+        ```
+        java -jar fmudriver.jar -h
+        ```
+        """
+
+class CrossChecker(
+        private val fmuDir: File,
+        resultDir: File
+) {
+
+    private var failedOrRejected = false
+
+    private val fmuPath = fmuDir.listFiles().find {
+        it.name.endsWith(".fmu")
+    } ?: throw IllegalArgumentException("No .fmu file present in the FMU dir!")
+
+    private val refData = fmuDir.listFiles().find {
+        it.name.endsWith("ref.csv")
+    } ?: throw IllegalArgumentException("No *_ref.csv file present in FMU dir!")
+
+    private val defaultsData = fmuDir.listFiles().find {
+        it.name.endsWith(".opt")
+    } ?: throw IllegalArgumentException("No .opt file present in FMU dir!")
+
+    private val outputFolder = File(resultDir, getDefaultOutputDir(fmuDir)).apply {
+        if (!exists()) {
+            mkdirs()
+        }
+    }.absolutePath
+
+    private val options: DriverOptions by lazy{
+
+        val variables = parseVariables(refData)
+        val defaults = XcOptions.parse(defaultsData.readText())
+
+        DriverOptions(
+                startTime = defaults.startTime,
+                stopTime = defaults.stopTime,
+                stepSize = defaults.stepSize,
+
+                outputVariables = variables,
+                outputFolder = outputFolder,
+
+                failOnLargeSize = true
+        )
+    }
+
+    private fun reject(reason: String) {
+        File(outputFolder, "rejected").apply {
+            createNewFile()
+            writeText(reason)
+        }
+        LOG.warn("Rejected FMU '$fmuPath'. Reason: $reason")
+        failedOrRejected = true
+    }
+
+    private fun fail(reason: String) {
+        File(outputFolder, "failed").apply {
+            createNewFile()
+            writeText(reason)
+        }
+        LOG.warn("Failed to handle FMU '$fmuPath'. Reason: $reason")
+        failedOrRejected = true
+    }
+
+    private fun pass() {
+        File(outputFolder, "passed").apply {
+            createNewFile()
+        }
+        LOG.info("FMU '$fmuPath' passed crosscheck")
+    }
+
+    private fun run(): Boolean {
+
+        val inputData = fmuDir.listFiles().find {
+            it.name.endsWith("in.csv")
+        }
+
+        var md: ModelDescriptionProvider? = null
+        try {
+            md = JacksonModelDescriptionParser.parse(fmuPath)
+        } catch (ex: Exception) {
+            LOG.error("Failed to parse model description!", ex)
+            fail("Failed to parse model description!")
+        }
+
+        try {
+
+            val fileName = fmuPath.absolutePath.replace("\\", "/")
+            blacklist.find { fileName.contains(it.toRegex()) }?.also {
+                reject("FMU has been blacklisted")
+            }
+
+            when {
+                refData.length() > 1E6 -> reject("Reference data > 1MB")
+                OS.LINUX.isCurrentOs && "JModelica.org" in fmuDir.absolutePath -> reject("JModelica.org FMUs makes the LInux system crash.")
+                options.stepSize < 0 -> reject("Invalid stepSize (stepSize < 0).")
+                options.startTime >= options.stopTime -> reject("Invalid start and or stop time (startTime >= stopTime).")
+                options.stepSize == 0.0 -> fail("Don't know how to handle variable step solver (stepsize=0.0).")
+                inputData != null -> fail("Unable to handle input files yet.")
+                md!!.asCoSimulationModelDescription().attributes.needsExecutionTool -> reject("FMU requires execution tool.")
+
+            }
+
+            if (!failedOrRejected) {
+                FmuDriver(fmuPath, options).run()
+                pass()
+            }
+
+        } catch (ex: Exception) {
+
+            when (ex) {
+                is Rejection -> reject(ex.reason)
+                is Failure -> fail(ex.reason)
+                else -> fail("Program error: $ex")
+            }
+        }
+
+        File(outputFolder, "README.md").apply {
+            writeText(README)
+        }
+
+        return !failedOrRejected
+
+    }
+
+    companion object {
+
+        private val LOG: Logger = LoggerFactory.getLogger(CrossChecker::class.java)
+
+        private val platform = when {
+            OS.LINUX.isCurrentOs -> "linux64"
+            OS.WINDOWS.isCurrentOs -> "win64"
+            else -> throw AssertionError("Invalid platform '${OsUtil.currentOS}'..")
+        }
+
+        private val blacklist = listOf<String>(
+//                "MapleSim/2015.1/Rectifier/Rectifier.fmu",
+//                "MapleSim/2015.2/Rectifier/Rectifier.fmu",
+//                "MapleSim/2016.1/Rectifier/Rectifier.fmu",
+//                "MapleSim/2016.2/Rectifier/Rectifier.fmu",
+//                "MapleSimH2018/Rectifier/Rectifier.fmu",
+//                "MWorks/2016/Rectifier/Rectifier.fmu",
+//                "MWorks/2016/fullRobot/fullRobot.fmu",
+//                "MWorks/2016/DFFREG/DFFREG.fmu",
+//                "MWorks/2016/DFFREG/DFFREG.fmu",
+//                "SimulationX/3.7.41138/Engine1b/Engine1b.fmu"
+        )
+
+        private fun parseVariables(refData: File): List<String>{
+            val txt = refData.reader().buffered().readLine()
+            return txt.split(",").let {
+                it.subList(1, it.size).map { it.replace("^\"|\"$".toRegex(), "").trim() }
+            }
+        }
+
+        fun run(crossCheckDir: String) {
+
+            val csPath = File("$crossCheckDir/fmus/2.0/cs/$platform")
+
+            File("$crossCheckDir/results/2.0/cs/$platform/FMI4j/$FMI4j_VERSION").apply {
+                if (exists()) deleteRecursively()
+            }
+
+            File("$crossCheckDir/results/2.0/me/$platform/FMI4j/$FMI4j_VERSION").apply {
+                if (exists()) deleteRecursively()
+            }
+
+            fun crosscheck(dir: File): Int {
+
+                val numPassed = AtomicInteger(0)
+
+                val fmus = mutableListOf<File>();
+                dir.listFiles().forEach { vendor ->
+                    vendor.listFiles().forEach { version ->
+                        version.listFiles().forEach { fmu ->
+                            fmus.add(fmu)
+
+                        }
+                    }
+
+                }
+
+                fmus.stream().forEach { fmu ->
+                    if (CrossChecker(fmu, File(crossCheckDir, "results")).run()) {
+                        numPassed.incrementAndGet()
+                    }
+                }
+
+                return numPassed.get()
+            }
+
+            LOG.info("${crosscheck(csPath)} Co-simulation FMUs passed cross-check")
+
+            cleanup()
+        }
+
+        private fun cleanup() {
+
+            listOf(
+                    "startvalues.txt",
+                    "vanDerPol.simulation.ezanl",
+                    "vanDerPol.simulation.ezapl",
+                    "vanDerPol.simulation.ezrpd",
+                    "vanDerPol.simulation_old.ezapl",
+                    "vanDerPol.simulation_old.ezrpd"
+            ).forEach {
+                File(it).apply {
+                    if (exists()) {
+                        delete()
+                    }
+                }
+            }
+
+        }
+
+        private fun getDefaultOutputDir(fmuFile: File): String {
+            var currentFile = fmuFile
+            var names = mutableListOf<String>()
+            for (i in 0..2) {
+                names.add(currentFile.name)
+                currentFile = currentFile.parentFile
+            }
+
+            names.addAll(listOf(FMI4j_VERSION, "FMI4j"))
+
+            for (i in 0..2) {
+                names.add(currentFile.name)
+                currentFile = currentFile.parentFile
+            }
+            return names.reverse().let { names.joinToString("/") }
+        }
+
+    }
+
+}
+
+fun main(args: Array<String>) {
+
+    if (args.size != 1) {
+        throw IllegalArgumentException("Missing path to fmi-cross-check folder!")
+    }
+
+    CrossChecker.run(args[0])
+
+}
