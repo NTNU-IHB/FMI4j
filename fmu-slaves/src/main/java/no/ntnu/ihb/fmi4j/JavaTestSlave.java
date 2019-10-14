9dc3f7f2c8513c924c11c2cd660fe567a06483cd
Lars Ivar Hatledal <laht@ntnu.no>
2019-10-14 09:33:14 +0200

    Revert changes that were not to be committed

--- fmu-slaves/src/main/java/no/ntnu/ihb/fmi4j/JavaTestSlave.java
+++ fmu-slaves/src/main/java/no/ntnu/ihb/fmi4j/JavaTestSlave.java
@@ -5,49 +5,17 @@ import no.ntnu.ihb.fmi4j.modeldescription.fmi2.Fmi2Variability;
 
 @SlaveInfo(
         modelName = "Test",
-        author = "Lars Ivar Hatledal",
-        version = "asdas"
+        author = "Lars Ivar Hatledal"
 )
 public class JavaTestSlave extends Fmi2Slave {
 
     @ScalarVariable(causality = Fmi2Causality.output, variability = Fmi2Variability.constant)
     protected double realOut = 2.0;
 
-    @ScalarVariable
-    Vector3 vector3;
-
-
-
     @Override
     public boolean doStep(double currentTime, double dt) {
         realOut += dt;
         return true;
-    }
-
-
-    static class Vector3 implements RealVector {
-
-        @Override
-        public int getSize() {
-            return 3;
-        }
-
-        @Override
-        public double get(int index) {
-            return 0;
-        }
-
-        @Override
-        public void set(int index, double value) {
-
-        }
-
-        @ScalarVariable
-        double x;
-
-        @ScalarVariable
-        double y;
-
     }
 
 }
