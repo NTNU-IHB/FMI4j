515fab6aec1e199ac040c364e441ca5fb44e1862
Lars Ivar Hatledal <laht@ntnu.no>
2019-10-14 09:16:20 +0200

    fix jitpack.yml

--- fmu-slaves/src/main/java/no/ntnu/ihb/fmi4j/JavaTestSlave.java
+++ fmu-slaves/src/main/java/no/ntnu/ihb/fmi4j/JavaTestSlave.java
@@ -1,29 +1,53 @@
 package no.ntnu.ihb.fmi4j;
 
 import no.ntnu.ihb.fmi4j.modeldescription.fmi2.Fmi2Causality;
+import no.ntnu.ihb.fmi4j.modeldescription.fmi2.Fmi2Variability;
 
 @SlaveInfo(
         modelName = "Test",
-        author = "Lars Ivar Hatledal"
+        author = "Lars Ivar Hatledal",
+        version = "asdas"
 )
 public class JavaTestSlave extends Fmi2Slave {
 
-    @ScalarVariable(causality = Fmi2Causality.output)
+    @ScalarVariable(causality = Fmi2Causality.output, variability = Fmi2Variability.constant)
     protected double realOut = 2.0;
 
-    @ScalarVariable(causality = Fmi2Causality.output)
-    protected int intOut = 99;
+    @ScalarVariable
+    Vector3 vector3;
 
-    @ScalarVariable(causality = Fmi2Causality.output)
-    protected double[] realsOut = {50.0, 200.0};
 
-    @ScalarVariable(causality = Fmi2Causality.local)
-    protected String[] string = {"Hello", "world!"};
 
     @Override
     public boolean doStep(double currentTime, double dt) {
         realOut += dt;
         return true;
+    }
+
+
+    static class Vector3 implements RealVector {
+
+        @Override
+        public int getSize() {
+            return 3;
+        }
+
+        @Override
+        public double get(int index) {
+            return 0;
+        }
+
+        @Override
+        public void set(int index, double value) {
+
+        }
+
+        @ScalarVariable
+        double x;
+
+        @ScalarVariable
+        double y;
+
     }
 
 }
