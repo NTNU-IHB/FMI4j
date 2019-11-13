aa301b109d5a6f77477f843ac1bcb672f031473f
Lars Ivar Hatledal <laht@ntnu.no>
2019-11-13 10:22:23 +0100

    Update test file

--- fmu-slaves/src/main/kotlin/no/ntnu/ihb/fmi4j/KotlinTestSlave.kt
+++ fmu-slaves/src/main/kotlin/no/ntnu/ihb/fmi4j/KotlinTestSlave.kt
@@ -5,8 +5,18 @@ class KotlinTestSlave: Fmi2Slave() {
     @ScalarVariable
     private val speed: Double = 10.0
 
+    @VariableContainer
+    private val data = Data()
+
     override fun doStep(currentTime: Double, dt: Double): Boolean {
         return true
     }
+
+}
+
+class Data {
+
+    @ScalarVariable
+    val x: Double = 0.0
 
 }
