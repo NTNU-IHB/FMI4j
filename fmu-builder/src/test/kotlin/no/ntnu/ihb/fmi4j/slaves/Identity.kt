712c3bdcbb63fc8cfa48078c59676c838d2fd128
Lars Ivar Hatledal <laht@hials.no>
2020-05-19 08:49:00 +0200

    Identity fmu

---
+++ fmu-builder/src/test/kotlin/no/ntnu/ihb/fmi4j/slaves/Identity.kt
@@ -0,0 +1,40 @@
+package no.ntnu.ihb.fmi4j.slaves
+
+import no.ntnu.ihb.fmi4j.FmuBuilder
+import no.ntnu.ihb.fmi4j.export.fmi2.Fmi2Slave
+import no.ntnu.ihb.fmi4j.export.fmi2.ScalarVariable
+import no.ntnu.ihb.fmi4j.modeldescription.fmi2.Fmi2Causality
+import java.io.File
+import java.io.FileFilter
+
+class Identity(
+        args: Map<String, Any>
+): Fmi2Slave(args) {
+
+    @ScalarVariable(causality = Fmi2Causality.output)
+    private var real = 0.0
+    @ScalarVariable(causality = Fmi2Causality.output)
+    private var integer = 0
+    @ScalarVariable(causality = Fmi2Causality.output)
+    private var boolean = false
+    @ScalarVariable(causality = Fmi2Causality.output)
+    private var string = ""
+
+    override fun doStep(currentTime: Double, dt: Double) {
+    }
+
+}
+
+
+fun main() {
+    val group = "no.ntnu.ihb.fmi4j.slaves"
+    val version = File("VERSION").readText()
+    val dest = File("build/generated").absolutePath
+    val jar = File("fmu-builder/build/libs").listFiles(FileFilter {
+        it.name == "fmu-slaves-$version.jar"
+    })?.get(0)?.absolutePath
+
+    jar?.also {
+        FmuBuilder.main(arrayOf("-f", it, "-m", "${group}.Identity", "-d", dest))
+    }
+}
