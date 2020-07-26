846cb73271831ffc24e733f605644e4a36a8aba5
Lars Ivar Hatledal <laht@hials.no>
2020-07-26 10:15:03 +0200

    plugin wip

--- fmu-plugin/src/main/kotlin/no/ntnu/ihb/fmi4j/FmuExportPlugin.kt
+++ fmu-plugin/src/main/kotlin/no/ntnu/ihb/fmi4j/FmuExportPlugin.kt
@@ -2,11 +2,13 @@ package no.ntnu.ihb.fmi4j
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.jvm.tasks.Jar
 import java.io.File
 
 open class FmuExportPluginExt {
 
-    var outputDir: File? = null
+    var outputDir: String? = null
+    var mainClass: String? = null
 
 }
 
@@ -15,8 +17,34 @@ class FmuExportPlugin: Plugin<Project> {
     override fun apply(project: Project) {
 
         project.apply {
+/*
+           project.tasks.create("fatJar", Jar::class.java).apply {
+                archiveBaseName.set("fatJar")
+                from (project.configurations.getByName("compile").map {
+                    if (it.isDirectory) it else project.zipTree(it)
+                })
+                with()
+            }*/
 
-            project.extensions.create("fmi4j", FmuExportPluginExt::class.java).also { ext ->
+            project.extensions.create("fmuExport", FmuExportPluginExt::class.java).also { ext ->
+
+                project.task("invoke").apply {
+
+                    group = "fmi4j"
+                    dependsOn += "fatJar"
+
+                    doLast {
+
+                        val outputDir: File? = ext.outputDir?.let { File(it) }
+
+                        FmuBuilder.main(arrayOf(
+                                "-m", ext.mainClass!!,
+                                "-f", ""
+                        ))
+
+                    }
+
+                }
 
             }
 
