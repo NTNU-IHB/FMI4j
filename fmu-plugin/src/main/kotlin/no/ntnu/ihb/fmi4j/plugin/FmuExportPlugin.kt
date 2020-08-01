9d829866f67b0ce0c987d796b387eb74aa77164c
Lars Ivar Hatledal <laht@ntnu.no>
2020-08-01 08:23:07 +0200

    Feature/export plugin (#110)

---
+++ fmu-plugin/src/main/kotlin/no/ntnu/ihb/fmi4j/plugin/FmuExportPlugin.kt
@@ -0,0 +1,81 @@
+package no.ntnu.ihb.fmi4j.plugin
+
+import no.ntnu.ihb.fmi4j.FmuBuilder
+import org.gradle.api.GradleException
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.DependencyResolutionListener
+import org.gradle.api.artifacts.ResolvableDependencies
+import org.gradle.api.file.CopySpec
+import org.gradle.jvm.tasks.Jar
+import java.io.File
+import java.net.URI
+
+open class FmuExportPluginExt {
+    var outputDir: String? = null
+    var mainClasses: MutableList<String> = mutableListOf()
+}
+
+class FmuExportPlugin: Plugin<Project> {
+
+    @Suppress("IMPLICIT_CAST_TO_ANY")
+    override fun apply(project: Project) {
+
+        val ext = project.extensions.create("fmi4jExport", FmuExportPluginExt::class.java)
+
+        project.tasks.register("fatJar", Jar::class.java) {
+            it.archiveBaseName.set("${project.name}_shadow")
+            it.from(project.configurations.getByName("compile").map { file ->
+                if (file.isDirectory) file else project.zipTree(file)
+            })
+            it.with(project.tasks.getByName("jar") as CopySpec)
+        }
+
+        project.task("exportFmu").apply {
+
+            group = "fmi4j"
+            dependsOn("fatJar")
+
+            doLast {
+
+                if (ext.mainClasses.isEmpty()) {
+                    throw GradleException("No mainClass(es) defined!")
+                }
+
+                val defaultOutputDir by lazy {
+                    File(project.buildDir, "fmus").apply {
+                        mkdirs()
+                    }
+                }
+                val outputDir = ext.outputDir ?: defaultOutputDir.absolutePath
+
+                for (mainClass in ext.mainClasses) {
+
+                    val args = arrayOf(
+                            "-f", "${project.buildDir}/libs/${project.name}_shadow.jar",
+                            "-m", mainClass,
+                            "-d", outputDir
+                    )
+                    FmuBuilder.main(args)
+
+                }
+            }
+        }
+
+        project.repositories.maven {
+            it.url=URI("https://dl.bintray.com/ntnu-ihb/mvn")
+        }
+        project.gradle.addListener(object: DependencyResolutionListener {
+
+            override fun beforeResolve(deps: ResolvableDependencies) {
+                val compileDefs = project.configurations.getByName("compile").dependencies
+                compileDefs.add(project.dependencies.create("no.ntnu.ihb.fmi4j:fmi-export:0.31.3"))
+                project.gradle.removeListener(this)
+            }
+
+            override fun afterResolve(deps: ResolvableDependencies) {
+
+            }
+        })
+    }
+}
---
+++ fmu-plugin/src/test/kotlin/no/ntnu/ihb/fmi4j/ApplyPluginTest.kt
@@ -0,0 +1,95 @@
+package no.ntnu.ihb.fmi4j
+
+import org.gradle.internal.impldep.org.junit.rules.TemporaryFolder
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import java.io.File
+
+internal class ApplyPluginTest {
+
+    @Test
+    @Disabled
+    fun testApplyPlugin() {
+
+        val testProjectDir = TemporaryFolder().apply { create() }
+
+        testProjectDir.newFile("settings.gradle").apply {
+            writeText("""
+                pluginManagement {
+                    repositories {
+                        mavenCentral()
+                        mavenLocal()
+                    }
+                }
+                rootProject.name = "testPlugin"
+            """.trimIndent())
+        }
+        testProjectDir.newFile("build.gradle").apply {
+            writeText("""
+                plugins {
+                    id "java-library" 
+                    id "no.ntnu.ihb.fmi4j.fmu-export" version "0.31.3"
+                }
+                
+                configurations.all {
+                    // Check for updates every build
+                    resolutionStrategy.cacheChangingModulesFor 0, 'seconds'
+                }
+                
+                repositories {
+                    mavenCentral()
+                    mavenLocal()
+                }
+
+                dependencies {
+                    implementation group: 'com.google.code.gson', name: 'gson', version: '2.8.6'
+                }
+                
+                fmi4jExport {
+                    mainClasses += "no.ntnu.ihb.MySlave"
+                }
+            """.trimIndent())
+        }
+
+        testProjectDir.newFolder("src", "main", "java", "no", "ntnu", "ihb")
+        testProjectDir.newFile("src/main/java/no/ntnu/ihb/MySlave.java").apply {
+            writeText("""
+                package no.ntnu.ihb;
+                
+                import java.util.Map;
+                import no.ntnu.ihb.fmi4j.export.fmi2.Fmi2Slave;
+                import no.ntnu.ihb.fmi4j.export.fmi2.ScalarVariable;
+                import no.ntnu.ihb.fmi4j.modeldescription.fmi2.Fmi2Causality;
+                
+                public class MySlave extends Fmi2Slave {
+                
+                    @ScalarVariable(causality = Fmi2Causality.output)
+                    double realOut = 1.0;
+                    
+                    public MySlave(Map<String, Object> args) {
+                        super(args);
+                    }
+                
+                    @Override
+                    public void doStep(double t, double dt) {
+                    }
+                
+                }
+            """.trimIndent())
+        }
+
+        val taskName = "exportFmu"
+        val result = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments(taskName)
+                .build()
+
+        Assertions.assertEquals(TaskOutcome.SUCCESS, result.task(":$taskName")?.outcome)
+        Assertions.assertTrue(File(testProjectDir.root, "build/fmus/MySlave.fmu").exists())
+
+    }
+
+}
