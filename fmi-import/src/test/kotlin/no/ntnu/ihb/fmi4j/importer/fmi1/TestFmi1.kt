985ec28d9dabb67642eb80d61c131bfd17e297c5
Lars Ivar Hatledal <laht@ntnu.no>
2020-08-11 09:18:23 +0200

    Refactor tests

--- fmi-import/src/test/kotlin/no/ntnu/ihb/fmi4j/importer/fmi1/TestFmi1.kt
+++ fmi-import/src/test/kotlin/no/ntnu/ihb/fmi4j/importer/fmi1/TestFmi1.kt
@@ -7,18 +7,17 @@ import no.ntnu.ihb.fmi4j.readReal
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.condition.EnabledOnOs
+import org.junit.jupiter.api.condition.OS
 import java.io.File
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class TestFmi1 {
 
-    private companion object {
-        private val file = TestFMUs.get("1.0/cs/BouncingBall.fmu")
-    }
-
     @Test
+    @EnabledOnOs(OS.WINDOWS)
     fun TestFmi1() {
 
+        val file = TestFMUs.get("1.0/cs/BouncingBall.fmu")
         Assertions.assertEquals("1.0", ModelDescriptionParser.extractVersion(file))
 
         Fmu.from(file).asCoSimulationFmu().use { fmu ->
@@ -46,8 +45,10 @@ internal class TestFmi1 {
     }
 
     @Test
+    @EnabledOnOs(OS.WINDOWS)
     fun testAbstractFmuLoad() {
 
+        val file = TestFMUs.get("1.0/cs/BouncingBall.fmu")
         AbstractFmu.from(file).asCoSimulationFmu().use { fmu ->
 
             fmu.newInstance(fmu.modelDescription.attributes.modelIdentifier).use { slave ->
@@ -56,6 +57,20 @@ internal class TestFmi1 {
 
         }
 
+    }
+
+    @Test
+    fun testIdentity() {
+        val file = TestFMUs.get("1.0/cs/identity.fmu")
+        AbstractFmu.from(file).asCoSimulationFmu().use { fmu ->
+
+            fmu.newInstance().use { slave ->
+                Assertions.assertTrue(slave.simpleSetup())
+                Assertions.assertTrue(slave.doStep(0.1))
+                Assertions.assertTrue(slave.terminate())
+            }
+
+        }
     }
 
 }
