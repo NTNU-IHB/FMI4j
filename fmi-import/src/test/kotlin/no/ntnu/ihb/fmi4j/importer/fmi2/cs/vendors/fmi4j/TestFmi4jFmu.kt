1cc5ebaa39ed85fb5474a5147c6d8089adae14b1
Lars Ivar Hatledal <laht@hials.no>
2020-09-23 09:04:02 +0200

    test proxy fmu

---
+++ fmi-import/src/test/kotlin/no/ntnu/ihb/fmi4j/importer/fmi2/cs/vendors/fmi4j/TestFmi4jFmu.kt
@@ -0,0 +1,24 @@
+package no.ntnu.ihb.fmi4j.importer.fmi2.cs.vendors.fmi4j
+
+import no.ntnu.ihb.fmi4j.TestFMUs
+import no.ntnu.ihb.fmi4j.importer.fmi2.Fmu
+import org.junit.jupiter.api.Test
+
+internal class TestFmi4jFmu {
+
+    @Test
+    fun testFmi4jFmu() {
+
+        val fmuFile = TestFMUs.get("2.0/cs/fmi4j/KotlinTestFmi2Slave.fmu")
+
+        Fmu.from(fmuFile).use { fmu ->
+            fmu.asCoSimulationFmu().newInstance().use { slave ->
+                slave.simpleSetup()
+                slave.doStep(0.1)
+                slave.terminate()
+            }
+        }
+
+    }
+
+}
