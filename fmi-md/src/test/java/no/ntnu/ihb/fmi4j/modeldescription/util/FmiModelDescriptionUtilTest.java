1565f12ad7c8ce19337ce0c939102b97dc72d505
Lars Ivar Hatledal <laht@hials.no>
2020-11-08 20:52:04 +0100

    lowercase..

--- fmi-md/src/test/java/no/ntnu/ihb/fmi4j/modeldescription/util/FmiModelDescriptionUtilTest.java
+++ fmi-md/src/test/java/no/ntnu/ihb/fmi4j/modeldescription/util/FmiModelDescriptionUtilTest.java
@@ -17,7 +17,7 @@ class FmiModelDescriptionUtilTest {
             Assertions.assertEquals("1.0", FmiModelDescriptionUtil.extractVersion(fmu));
         }
         {
-            File fmu = TestFMUs.get("2.0/cs/20Sim/4.6.4.8004/ControlledTemperature/ControlledTemperature.fmu");
+            File fmu = TestFMUs.get("2.0/cs/20sim/4.6.4.8004/ControlledTemperature/ControlledTemperature.fmu");
             Assertions.assertTrue(fmu.exists(), "No such file: " + fmu.getAbsolutePath());
             Assertions.assertEquals("2.0", FmiModelDescriptionUtil.extractVersion(fmu));
         }
@@ -31,7 +31,7 @@ class FmiModelDescriptionUtilTest {
             Assertions.assertEquals("{8c4e810f-3da3-4a00-8276-176fa3c9f000}", FmiModelDescriptionUtil.extractGuid(fmu));
         }
         {
-            File fmu = TestFMUs.get("2.0/cs/20Sim/4.6.4.8004/ControlledTemperature/ControlledTemperature.fmu");
+            File fmu = TestFMUs.get("2.0/cs/20sim/4.6.4.8004/ControlledTemperature/ControlledTemperature.fmu");
             Assertions.assertTrue(fmu.exists(), "No such file: " + fmu.getAbsolutePath());
             Assertions.assertEquals("{06c2700b-b39c-4895-9151-304ddde28443}", FmiModelDescriptionUtil.extractGuid(fmu));
         }
