8c522cf3e2b9559f0f3fc30754e0de682a41723c
Lars Ivar Hatledal <laht@ntnu.no>
2020-11-09 10:37:17 +0100

    add test

---
+++ fmi-md/src/test/java/no/ntnu/ihb/fmi4j/modeldescription/fmi1/TestFmiModelDescription.java
@@ -0,0 +1,25 @@
+package no.ntnu.ihb.fmi4j.modeldescription.fmi1;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TestFmiModelDescription {
+
+    @Test
+    public void testVesselFmu() throws IOException {
+
+        File xml = new File(TestFmiModelDescription.class.getClassLoader()
+                .getResource("fmi1.0/VesselFmu/modelDescription.xml").getFile());
+
+        XmlMapper mapper = new XmlMapper();
+        FmiModelDescription md = mapper.readValue(xml, FmiModelDescription.class);
+
+        Assertions.assertNotNull(md.getImplementation().getCoSimulationStandAlone().capabilities);
+
+    }
+
+}
