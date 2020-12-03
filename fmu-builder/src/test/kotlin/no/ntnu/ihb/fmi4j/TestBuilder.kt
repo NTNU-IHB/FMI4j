2303fab3c6a23ec5c2542be7b57a6a78cc935fd7
Lars Ivar Hatledal <laht@ntnu.no>
2020-12-03 13:08:55 +0100

    Add test

--- fmu-builder/src/test/kotlin/no/ntnu/ihb/fmi4j/TestBuilder.kt
+++ fmu-builder/src/test/kotlin/no/ntnu/ihb/fmi4j/TestBuilder.kt
@@ -94,6 +94,64 @@ internal class TestBuilder {
     }
 
     @Test
+    fun testIdentity() {
+        FmuBuilder.main(
+            arrayOf(
+                "-m", "$group.Identity",
+                "-f", jar,
+                "-d", dest
+            )
+        )
+
+        val fmuFile = File(dest, "Identity.fmu")
+        Assertions.assertTrue(fmuFile.exists())
+
+        val vrs = longArrayOf(0)
+
+        val intValue = intArrayOf(99)
+        val realValue = doubleArrayOf(12.3)
+        val boolValue = booleanArrayOf(true)
+        val strValue = stringArrayOf("Hello identity")
+
+        val intRef = IntArray(1)
+        val realRef = DoubleArray(1)
+        val boolRef = BooleanArray(1)
+        val strRef = StringArray(1)
+
+        Fmu.from(fmuFile).asCoSimulationFmu().use { fmu ->
+
+            val md = fmu.modelDescription
+            Assertions.assertEquals("Identity", md.modelName)
+
+            fmu.newInstance().use { slave ->
+
+                Assertions.assertTrue(slave.simpleSetup())
+
+                slave.writeInteger(vrs, intValue)
+                slave.writeReal(vrs, realValue)
+                slave.writeBoolean(vrs, boolValue)
+                slave.writeString(vrs, strValue)
+
+                Assertions.assertTrue(slave.doStep(0.1))
+
+                slave.readInteger(vrs, intRef)
+                slave.readReal(vrs, realRef)
+                slave.readBoolean(vrs, boolRef)
+                slave.readString(vrs, strRef)
+
+                Assertions.assertEquals(intRef.first(), intValue.first())
+                Assertions.assertEquals(realRef.first(), realValue.first())
+                Assertions.assertEquals(boolRef.first(), boolValue.first())
+                Assertions.assertEquals(strRef.first(), strValue.first())
+
+                Assertions.assertTrue(slave.terminate())
+
+            }
+
+        }
+    }
+
+    @Test
     fun testGetAndSetAll() {
         FmuBuilder.main(
             arrayOf(
